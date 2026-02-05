6f41ef0751acac8625c957a00f2baa71cca78206
hacker-2004 <tirth.ampatel@gmail.com>
2026-02-05 12:02:22 -0500

    made custom textsize with custom adopter

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -1,25 +1,32 @@
 package edu.temple.inclassuiacvitivity
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 
-class TextSizeAdapter : BaseAdapter() {
+class TextSizeAdapter (_context: Context, _size: Array<String>) : BaseAdapter() {
+    private val context = _context
+    private val size = _size
 
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+        return size.size
     }
 
     override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
+        return size[position]
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
+        return position.toLong()
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+        val textView = TextView(context)
+        textView.text = size[position]
+        textView.textSize = size[position].toFloat()
+        return textView
     }
 
 }
